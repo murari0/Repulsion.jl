a339dcd9fe4fcc3b0e69bd84bfeec56d211fc5c1
Frederic Mentink-Vigier <75385132+fmentink@users.noreply.github.com>
2021-03-23 17:52:02 -0400

    Update repulsion.jl

--- repulsion.jl
+++ repulsion.jl
@@ -2,46 +2,7 @@ using LinearAlgebra
 using StaticArrays
 using Quaternions
 
-function toangles(p::SVector{N,T}) where N where T
-    angles = zeros(3)
-    if N == 2
-        angles[2] = atan(p[2], p[1])
-    elseif N == 3
-        angles[3] = atan(p[2], p[1])
-        angles[2] = pi/2 + atan(p[3], hypot(p[1], p[2]))
-    elseif N == 4
-        q = Quaternion(p...,true)
-        r = rotationmatrix(q)
-        # Quaternion to ZYZ Euler angles
-        angles[2] = acos(r[3,3])
-        if sin(angles[2]) == 0
-            angles[1] = 0
-            angles[3] = asin(r[2,1])
-            if angles[3] < 0
-                angles[3] = pi - angles[3]
-            end
-        else
-            angles[3] = atan2(r[2,3], r[1,3])
-            if angles[3] < 0
-                angles[3] = pi + angles[3]
-            end
-            angles[1] = atan2(r[3,2], -1*r[3,1])
-            if angles[1] < 0
-                angles[1] = pi + angles[1]
-            end
-        end
-    end
-    angles
-end
 
-function dist_vector(a::SVector{N,T}, b::SVector{N,T}) where N where T
-    dd = normalize(a - b)
-    if isnan(first(dd))
-        zeros(typeof(a))
-    else
-        dd*dot(a, b)
-    end
-end
 
 function repulsion(npoints, dims, niter = 1e7, convergence_tol::T = 1e-10) where {T <: AbstractFloat}
     R_raw = rand(T, dims, npoints) .- one(T)/2
@@ -65,4 +26,47 @@ function repulsion(npoints, dims, niter = 1e7, convergence_tol::T = 1e-10) where
         end
     end
     toangles.(R)
+end
+
+
+
+function dist_vector(a::SVector{N,T}, b::SVector{N,T}) where N where T
+    dd = normalize(a - b)
+    if isnan(first(dd))
+        zeros(typeof(a))
+    else
+        dd*dot(a, b)
+    end
+end
+
+function toangles(p::SVector{N,T}) where N where T
+    angles = zeros(3)
+    if N == 2
+	angles[2] = atan(p[2], p[1])
+    elseif N == 3
+	angles[3] = atan(p[2], p[1])
+	angles[2] = pi/2 + atan(p[3], hypot(p[1], p[2]))
+    elseif N == 4
+	q = Quaternion(p...,true)
+	r = rotationmatrix(q)
+	# Quaternion to ZYZ Euler angles
+	angles[2] = acos(r[3,3])
+	if sin(angles[2]) == 0
+	    angles[1] = 0
+	    angles[3] = asin(r[2,1])
+	    if angles[3] < 0
+		angles[3] = pi - angles[3]
+	    end
+	else
+	    angles[3] = atan2(r[2,3], r[1,3])
+	    if angles[3] < 0
+		angles[3] = pi + angles[3]
+	    end
+	    angles[1] = atan2(r[3,2], -1*r[3,1])
+	    if angles[1] < 0
+		angles[1] = pi + angles[1]
+	    end
+	end
+    end
+    angles
 end
